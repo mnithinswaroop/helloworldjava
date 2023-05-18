2ff35a84475b0cfb12be3ffa8cae2a5498ef3c3f
mnithinswaroop <99521282+mnithinswaroop@users.noreply.github.com>
2023-05-18 11:07:17 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World - May 10000bc 2023 - 11:55PM - triggered by webhook");
+    System.out.println("Hello World - May 10000BC 2023 - 11:55PM - triggered by webhook");
     }
 }
